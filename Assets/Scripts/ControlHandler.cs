2987596da47c9b251fc7d15444fa233140087fcc
Wachirawit Soontornworachan <pud.wachirawit@gmail.com>
2026-08-17 17:52:04 +0700

    Import InputSystem and test-run it

---
+++ Assets/Scripts/ControlHandler.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControlHandler : MonoBehaviour
+{
+    public GameObject pointerObject;
+
+    public Vector3 worldPointerPos;
+
+    InputAction mousePosAction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mousePosAction = InputSystem.actions.FindAction("Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = mousePosAction.ReadValue<Vector2>();
+        worldPointerPos = Camera.main.ScreenToWorldPoint(mousePos);
+        worldPointerPos.Scale(new Vector3(1, 1, 0));
+        pointerObject.transform.position = worldPointerPos;
+    }
+}
